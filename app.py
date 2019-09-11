d6b247d2f02b3fcb8ef8b6639e99b0178ee5aaf7
AskmienFoodoe <bowen.jiang@duke.edu>
2019-09-11 00:20:51 -0400

    Add CORS dependencies, eliminate circular import

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 api = Api(app)
 
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+# @app.before_first_request
+# def create_tables():
+#     db.create_all()
 
 
 api.add_resource(Score, '/scores')
